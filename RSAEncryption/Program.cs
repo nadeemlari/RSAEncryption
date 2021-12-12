aaeaa795c3edd73885389201a600d26515a41112
Nadeem Lari <nadeemlari@gmail.com>
2021-12-12 13:40:45 +0530

    Initial commit

---
+++ RSAEncryption/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using RSAEncryption;
+var rsa = new RsaEncryption();
+Console.WriteLine("Ender text to encrypt");
+var plain = Console.ReadLine();
+var cypher = RsaEncryption.Encrypt(plain);
+Console.WriteLine($"Encrypted data :{cypher}");
+Console.WriteLine($"Decrypted value:{rsa.Decrypt(cypher)}");
+
---
+++ RSAEncryption/RsaEncryption.cs
@@ -0,0 +1,41 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace RSAEncryption;
+public class RsaEncryption
+{
+    private static readonly RSACryptoServiceProvider Csp = new(2048);
+    private RSAParameters _privateKey;
+    private readonly RSAParameters _publicKey;
+
+    public RsaEncryption()
+    {
+        _privateKey = Csp.ExportParameters(true);
+        _publicKey = Csp.ExportParameters(false);
+    }
+    public string GetPublicKey()
+    {
+        var sw = new StringWriter();
+        var xs = new XmlSerializer(typeof(RSAParameters));
+        xs.Serialize(sw, _publicKey);
+        return sw.ToString();
+    }
+
+    public static string Encrypt(string? plainText)
+    {
+        if (plainText == null) return string.Empty;
+        var data = Encoding.Unicode.GetBytes(plainText);
+        var cypher = Csp.Encrypt(data,false);
+        return Convert.ToBase64String(cypher);
+
+    }
+
+    public string Decrypt(string cypher)
+    {
+        var dataInBytes = Convert.FromBase64String(cypher);
+        var plainText = Csp.Decrypt(dataInBytes, false);
+        return Encoding.Unicode.GetString(plainText);
+    }
+
+}
